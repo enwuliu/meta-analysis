0789bc52918163cc531f8959c503b859bc261f99
enwuliu <46331979+enwuliu@users.noreply.github.com>
2024-11-28 16:01:36 +1030

    Update Paper all R code.R

--- Paper all R code.R
+++ Paper all R code.R
@@ -1,149 +1,186 @@
-### R function for random effect model meta-analysis
+studies <- read.csv('https://raw.githubusercontent.com/enwuliu/meta-analysis/main/random_effect_meta_sim.csv', header = TRUE)
+b1 <- studies$b1  # beta1 from the 10 cohorts
+var_b1 <- studies$var_b1  # variances of the coefficients
 
-random_effect_meta<-function(B,V){
-  W<-1/V                                #Inverse variance weight
-  Q<-sum(W*B^2)-(sum(W*B))^2/sum(W)     #total variance or Q statistics
-  df<-length(B)-1                       #degree of freedom, number of studies minus 1
-  c<-sum(W)-sum(W^2)/sum(W)             #c is the scaling factor
-
-  tau_square<-ifelse(Q>df,(Q-df)/c,0)   #between study variance
-
-  V_star=V+tau_square
-  W_star=1/V_star                        #new weight
-  Beta_star<-sum(W_star*B)/sum(W_star)   #overall random effect coefficients
-  Var_star=1/sum(W_star)                 #ovall variance
-  upper_b<-Beta_star+1.96*sqrt(Var_star) #upper 95% CI for overall beta
-  lower_b<-Beta_star-1.96*sqrt(Var_star) #lower 95% CI for overall beta
-
-  resultlist<-list('Overal beta'=Beta_star,'Overall variance'=Var_star,'HR'=exp(Beta_star),'HR lower 95% CI'=exp(lower_b),'HR upper 95% CI'=exp(upper_b))#change to hazard ratio
+fixed_effect_meta <- function(B, V) {
+  W <- 1 / V
+  Beta <- sum(W * B) / sum(W)
+  Var <- 1 / sum(W)
+  resultlist <- list('Overall beta' = Beta, 'Overall variance' = Var)
   return(resultlist)
 }
 
-# Random meta analyis for beta1
-studies<-read.csv('https://raw.githubusercontent.com/enwuliu/meta-analysis/main/random_effect_meta_sim.csv',header=T)
-b1<-studies$b1 #beta1 of the 10 cohorts
-var_b1<-studies$var_b1 #variances of the coefficients
-meta1<-random_effect_meta(b1,var_b1) # random effect meta-analysis for coefficient beta1
-meta1
+random_effect_meta <- function(B, V) {
+  W <- 1 / V
+  Q <- sum(W * B^2) - (sum(W * B))^2 / sum(W)
+  df <- length(B) - 1
+  c <- sum(W) - sum(W^2) / sum(W)
+  tau_square <- ifelse(Q > df, (Q - df) / c, 0)
+  V_star <- V + tau_square
+  W_star <- 1 / V_star
+  Beta_star <- sum(W_star * B) / sum(W_star)
+  Var_star <- 1 / sum(W_star)
+  resultlist <- list('Overall beta' = Beta_star, 'Overall variance' = Var_star)
+  return(resultlist)
+}
 
-#### using the meta package to conduct random meta for beta1
-library(meta)
-m.gen<- metagen(TE = b1,
-                seTE = sqrt(var_b1),
-                studlab = LETTERS[1:10],
-                data = ,
-                sm = "HR",
-                method.tau = "DL",
-                fixed = FALSE,
-                random = TRUE,
-                title = "Use R meta package")
+fixed_b1 <- fixed_effect_meta(b1, var_b1)
+fixed_b1
 
-summary(m.gen)
+# $`Overall beta`
+# [1] 1.040411
+#
+# $`Overall variance`
+# [1] 0.6841809
 
-#forest plot for beta1
-forest.meta(m.gen, layout = "RevMan5")
+random_b1 <- random_effect_meta(b1, var_b1)
+random_b1
 
-# random effect meta-analysis for beta2 using function written by the authors
+# $`Overall beta`
+# [1] 1.014141
+#
+# $`Overall variance`
+# [1] 0.7308388
 
-b2<-studies$b2 #beta2 of the 10 cohorts
-var_b2<-studies$var_b2 #variances of the coefficients
-meta2<-random_effect_meta(b2,var_b2) # random effect meta-analysis for coefficient beta1
-meta2
+#################beta 2
 
-#### using the meta package to conduct random meta for beta2
+b2 <- studies$b2  # beta2 from the 10 cohorts
+var_b2 <- studies$var_b2  # variances of the coefficients
 
-m.gen2<- metagen(TE = b2,
-                 seTE = sqrt(var_b2),
-                 studlab = LETTERS[1:10],
-                 data = ,
-                 sm = "HR",
-                 method.tau = "DL",
-                 fixed = FALSE,
-                 random = TRUE,
-                 title = "Use R meta package")
+fixed_b2<-fixed_effect_meta(b2, var_b2)
+fixed_b2
+# $`Overall beta`
+# [1] -0.01140614
+#
+# $`Overall variance`
+# [1] 0.0001403961
 
-summary(m.gen2)
+random_b2<-random_effect_meta(b2, var_b2)
+random_b2
+# $`Overall beta`
+# [1] -0.01140614
+#
+# $`Overall variance`
+# [1] 0.0001403961
 
-#forest plot for beta2
 
-forest.meta(m.gen2, layout = "RevMan5")
+###meta analysis on r
 
-#function for condducting random effect meta-analysis for correlation coefficient r
+fixed_effect_meta_r<-function(v1,v2,cov12,sample_size){
+  r<-cov12/sqrt(v1*v2)                   #correlation coefficient
+  z<-0.5*log((1+r)/(1-r))                #Fisher z transformation
+  v<-1/(sample_size-3)                   #variance for z
+  W<-1/v                                 #weight
+  z_overall<-sum(W*z)/sum(W)             #overall random effect of z
+  r_overall<-(exp(2*z_overall)-1)/(exp(2*z_overall)+1) #transform back to r
+  return(r_overall)
+}
+random_effect_meta_r<-function(v1,v2,cov12,sample_size){
+  r<-cov12/sqrt(v1*v2)                   #correlation coefficient
+  z<-0.5*log((1+r)/(1-r))                #Fisher z transformation
+  v<-1/(sample_size-3)                   #variance for z
+  W<-1/v                                 #weight
+  Q<-sum(W*z^2)-(sum(W*z))^2/sum(W)      #total variance or Q statistics
+  c<-sum(W)-sum(W^2)/sum(W)              #c is the scaling factor
+  df=length(v1)-1                        #degree of freedom
+  tau_square<-ifelse(Q>df,(Q-df)/c,0)    #between study variance
 
-random_effect_meta_r<-function(V11,V22,Cov12,S_size){
-  r<-Cov12/sqrt(V11*V22)           #correlation coefficient
-  z<-0.5*log((1+r)/(1-r))          #Fisher z transformation
-  V<-1/(S_size-3)                  #variance for z
-  W<-1/V                            #weight
-  Q<-sum(W*z^2)-(sum(W*z))^2/sum(W) #total variance or Q statistics
-  df<-length(z)-1                   #degree of freedom, number of studies minus 1
-  c<-sum(W)-sum(W^2)/sum(W)         #c is the scaling factor
-
-  tau_square<-ifelse(Q>df,(Q-df)/c,0) #between study variance
-
-  V_star=V+tau_square
-  W_star=1/V_star                     #new weight
-  z_star<-sum(W_star*z)/sum(W_star)   #overall random effect of z
-  r_overall<-(exp(2*z_star)-1)/(exp(2*z_star)+1) #transform overall z* back to overall correlation coefficient
+  v_star=v+tau_square
+  W_star=1/v_star                        #new weight
+  z_star<-sum(W_star*z)/sum(W_star)      #overall random effect of z
+  r_overall<-(exp(2*z_star)-1)/(exp(2*z_star)+1) #transform back to r
   return(r_overall)
 }
 
-V11<-studies$var_b1
-V22<-studies$var_b2
-V12<-studies$cov_b1b2
-S_size<-studies$sample_size
+v1<-studies$var_b1
+v2<-studies$var_b2
+v12<-studies$cov_b1b2
+sample_size<-studies$sample_size
+r<-v12/sqrt(v1*v2)
+fixed_meta_r<-fixed_effect_meta_r(v1,v2,v12,sample_size)
+fixed_meta_r
+#[1] -0.9600286
+random_meta_r<-random_effect_meta_r(v1,v2,v12,sample_size)
+random_meta_r
+#[1] -0.9493409
 
-meta3<-random_effect_meta_r(V11,V22,V12,S_size)
-meta3  #overall correlation coefficient
 
-## using meta package to perfomr random effect meta-analysis on correlation coefficient r
+####plot
 
-r<-V12/sqrt(V11*V22) #
-m.cor <- metacor(cor = r, 
-                 n = S_size,
-                 studlab = LETTERS[1:10],
-                 data = ,
-                 fixed = FALSE,
-                 random = TRUE,
-                 method.tau = "DL",
-                 hakn = FALSE,
-                 title = "Use R meta package")
-summary(m.cor)
+library(ggplot2)
+library(ggpubr)
 
-#calculate covariance of beta1 and beta2
-cov_b1b2<-meta3*sqrt(meta1$`Overall variance`*meta2$`Overall variance`)
-cov_b1b2
+plot_with_interaction <- function(age, b1, v1, b2, v2, r) {
+  cov_b1b2 <- r * sqrt(v1 * v2)
+  RR <- exp(b1 + b2 * age)
+  RR_lower <- exp((b1 + b2 * age) - 1.96 * sqrt(v1 + age^2 * v2 + 2 * age * cov_b1b2))
+  RR_upper <- exp((b1 + b2 * age) + 1.96 * sqrt(v1 + age^2 * v2 + 2 * age * cov_b1b2))
+  ndata <- as.data.frame(cbind(RR, RR_lower, RR_upper, age))
+  ggplot(data = ndata, aes(x = age)) +
+    geom_line(aes(y = RR)) +
+    geom_line(aes(y = RR_lower), color = "steelblue", linetype = "dashed") +
+    geom_line(aes(y = RR_upper), color = "steelblue", linetype = "dashed") +
+    scale_y_continuous(name = "Rate Ratio (RR): Fallers vs. Non-Fallers",
+                       limits = c(0, 4), expand = c(0, 0)) +
+    theme_bw() +
+    theme(axis.line = element_line(color = 'black'),
+          axis.title.y = element_text(size = 8),
+          axis.title.x = element_text(size = 8),
+          axis.text.y = element_text(size = 8),
+          axis.text.x = element_text(size = 8),
+          plot.background = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank())
+}
 
-#plot figure 3 
-age<-seq(50,90,1)
-HR<-exp(meta1$`Overal beta`+meta2$`Overal beta`*age)
-HR_low<-exp((meta1$`Overal beta`+meta2$`Overal beta`*age)-1.96*sqrt(meta1$`Overall variance`+age^2*meta2$`Overall variance`+2*age*cov_b1b2))
-HR_upper<-exp((meta1$`Overal beta`+meta2$`Overal beta`*age)+1.96*sqrt(meta1$`Overall variance`+age^2*meta2$`Overall variance`+2*age*cov_b1b2))
-plot(age,HR,type='l',ylim=c(0,3.5),ylab='HR(fall vs. no fall)')
-lines(age,HR_low,type='l',col='blue',lty=2)
-lines(age,HR_upper,type='l',col='blue',lty=2)
+# Fixed-effect model
+age <- seq(50, 90, 1)
+b1 <- fixed_b1$`Overall beta`
+v1 <- fixed_b1$`Overall variance`
+b2 <- fixed_b2$`Overall beta`
+v2 <- fixed_b2$`Overall variance`
+r <- fixed_meta_r
 
-#overall beta1, beta2 and covaraince by multivariate meta-analysis
-library(mvmeta)
-library(dplyr)
-S<-as.matrix(select(sim_data,var_b1,cov_b1b2, var_b2))
-model<-mvmeta(cbind(sim_data$b1,sim_data$b2),S,method="ml")
-summary(model)
-model$vcov
+p1 <- plot_with_interaction(age, b1, v1, b2, v2, r)
+p1
+# Random-effect model
+b1_rand <- random_b1$`Overall beta`
+v1_rand <- random_b1$`Overall variance`
+b2_rand <- random_b2$`Overall beta`
+v2_rand <- random_b2$`Overall variance`
+r_rand <- random_meta_r
 
-# figure 4
+p2 <- plot_with_interaction(age, b1_rand, v1_rand, b2_rand, v2_rand, r_rand)
+p2
+p3 <- ggarrange(p1, p2,
+                labels = c("Fixed Effect", "Random Effect"),
+                ncol = 2, nrow = 1)
+p3
 
-cov<-model$vcov[1,2]
-beta<-model$coefficients
-HR_mv<-exp(beta[1]+beta[2]*age)
-HR_mv_low<-exp((beta[1]+beta[2]*age)-1.96*sqrt(model$vcov[1,1]+age^2*model$vcov[2,2]+2*age*cov))
-HR_mv_upper<-exp((beta[1]+beta[2]*age)+1.96*sqrt(model$vcov[1,1]+age^2*model$vcov[2,2]+2*age*cov))
-plot(age,HR_mv,type='l',ylim=c(0,8),ylab='HR(fall vs. no fall)',col='red',lwd=1.0,bty="n")
-lines(age,HR_mv_low,type='l',col='red',lty=4)
-lines(age,HR_mv_upper,type='l',col='red',lty=4)
-lines(age,HR,type='l',ylim=c(0,3.5),ylab='HR(fall vs. no fall)',col='blue',lwd=2.0)
-lines(age,HR_low,type='l',col='blue',lty=2)
-lines(age,HR_upper,type='l',col='blue',lty=2)
-legend(70, 8, c("Multivariate meta-analysis", "Univariate meta-analysis using r", "95% CI multivariate meta-analysis", "95% CI using r"), col=c("red", "blue","red","blue"), lty=c(1,1,4,2),cex=0.8, box.lty=0)
+#ggsave(filename = "E:\\cov_paper\\covmeta.jpeg",p3,units="mm", width = 250, height = 100,device='jpeg', dpi = 300)
+
+###### use the package
+library(devtools)
+install_github("enwuliu/covmeta")
+library(covmeta)
+
+# Load the dataset
+studies <- read.csv('https://raw.githubusercontent.com/enwuliu/meta-analysis/main/random_effect_meta_sim.csv', header = TRUE)
+
+# Function arguments
+b1 <- studies$b1          # Beta1 coefficients from the 10 cohorts
+v1 <- studies$var_b1      # Variances of Beta1
+b2 <- studies$b2          # Beta2 coefficients from the 10 cohorts
+v2 <- studies$var_b2      # Variances of Beta2
+cov_b1b2 <- studies$cov_b1b2  # Covariance between Beta1 and Beta2
+sample_size <- studies$sample_size  # Sample sizes
+
+# Calculate the overall main effect, interaction effect, and covariance
+# Fixed-effect meta-analysis
+cov_meta(b1, v1, b2, v2, cov_b1b2, sample_size, 'fixed')
+
+# Random-effect meta-analysis
+cov_meta(b1, v1, b2, v2, cov_b1b2, sample_size, 'random')
 
 
